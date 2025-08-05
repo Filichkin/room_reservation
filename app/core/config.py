e374f3e2168b882ab455738ade149d064f8bf09c
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-05 20:13:13 +0300

    start repo

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Бронирование переговорок'
+    description: str
+    database_url: str
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
