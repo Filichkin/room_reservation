8b9a94131f5bc7984752cb1e6a6a0c7e6271d7af
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-06 10:42:23 +0300

    added HTTPException

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from sqlalchemy import select
+
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
@@ -32,3 +36,15 @@ async def create_meeting_room(
         await session.refresh(db_room)
     # Возвращаем только что созданный объект класса MeetingRoom.
     return db_room
+
+
+async def get_room_id_by_name(room_name: str) -> Optional[int]:
+    async with AsyncSessionLocal() as session:
+        # Получаем объект класса Result.
+        db_room_id = await session.execute(
+            select(MeetingRoom.id).where(
+                MeetingRoom.name == room_name
+            )
+        )
+        db_room_id = db_room_id.scalars().first()
+    return db_room_id
