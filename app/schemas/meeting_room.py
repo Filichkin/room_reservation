8a0376619e349f03e984e4e0c7f850e7a8d3c765
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-06 11:06:39 +0300

    added MeetingRoomDB

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -3,15 +3,24 @@ from typing import Optional
 from pydantic import BaseModel, Field, field_validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(
-        ...,
-        max_length=100,
-    )
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    # Переопределяем атрибут name, делаем его обязательным.
+    name: str = Field(..., min_length=1, max_length=100)
 
     @field_validator('name')
     def name_validator(cls, value: str):
         if not value:
             raise ValueError('Имя не должно быть пустым')
         return value
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
