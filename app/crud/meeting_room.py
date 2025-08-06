74b9f357228dd69432805837304a56f47fee50b0
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-06 14:02:45 +0300

    added Depends(get_async_session)

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,17 +1,15 @@
 from typing import Optional
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
 
 
-# Функция работает с асинхронной сессией, 
-# поэтому ставим ключевое слово async.
-# В функцию передаём схему MeetingRoomCreate.
 async def create_meeting_room(
-        new_room: MeetingRoomCreate
+        new_room: MeetingRoomCreate,
+        session: AsyncSession,
 ) -> MeetingRoom:
     # Конвертируем объект MeetingRoomCreate в словарь.
     new_room_data = new_room.model_dump()
@@ -21,30 +19,20 @@ async def create_meeting_room(
     # для этого распаковываем словарь.
     db_room = MeetingRoom(**new_room_data)
 
-    # Создаём асинхронную сессию через контекстный менеджер.
-    async with AsyncSessionLocal() as session:
-        # Добавляем созданный объект в сессию.
-        # Никакие действия с базой пока ещё не выполняются.
-        session.add(db_room)
-
-        # Записываем изменения непосредственно в БД.
-        # Так как сессия асинхронная, используем ключевое слово await.
-        await session.commit()
-
-        # Обновляем объект db_room: считываем данные из БД,
-        # чтобы получить его id.
-        await session.refresh(db_room)
-    # Возвращаем только что созданный объект класса MeetingRoom.
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
     return db_room
 
 
-async def get_room_id_by_name(room_name: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        # Получаем объект класса Result.
-        db_room_id = await session.execute(
-            select(MeetingRoom.id).where(
-                MeetingRoom.name == room_name
-            )
+async def get_room_id_by_name(
+        room_name: str,
+        session: AsyncSession
+) -> Optional[int]:
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(
+            MeetingRoom.name == room_name
         )
-        db_room_id = db_room_id.scalars().first()
+    )
+    db_room_id = db_room_id.scalars().first()
     return db_room_id
