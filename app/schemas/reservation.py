58de98c87e6e6c8a64393e106e9aba274c0065e0
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-09 15:16:28 +0300

    upd ReservationBase

--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -1,11 +1,20 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 
-from pydantic import BaseModel, field_validator, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
+
+
+FROM_TIME = (
+    datetime.now() + timedelta(minutes=10)
+).isoformat(timespec='minutes')
+
+TO_TIME = (
+    datetime.now() + timedelta(hours=1)
+).isoformat(timespec='minutes')
 
 
 class ReservationBase(BaseModel):
-    from_reserve: datetime
-    to_reserve: datetime
+    from_reserve: datetime = Field(..., example=FROM_TIME)
+    to_reserve: datetime = Field(..., example=TO_TIME)
 
     model_config = {
         'extra': 'forbid'
@@ -16,7 +25,7 @@ class ReservationUpdate(ReservationBase):
 
     @field_validator('from_reserve')
     def check_from_reserve_later_than_now(cls, value):
-        if value <= datetime.now(timezone.utc):
+        if value <= datetime.now():
             raise ValueError(
                 'Время начала бронирования '
                 'не может быть меньше текущего времени'
