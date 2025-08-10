3e89a35e1c1211432ab44ef7559a4e9f2739eed5
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-10 10:56:26 +0300

    added user_router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import meeting_room_router, reservation_router
+from app.api.endpoints import (
+    meeting_room_router,
+    reservation_router,
+    user_router
+)
 
 
 main_router = APIRouter()
@@ -10,8 +14,11 @@ main_router.include_router(
     prefix='/meeting_rooms',
     tags=['Meeting Rooms']
     )
+
 main_router.include_router(
     reservation_router,
     prefix='/reservations',
     tags=['Reservations']
     )
+
+main_router.include_router(user_router)
