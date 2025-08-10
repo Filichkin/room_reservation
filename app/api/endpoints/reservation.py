0e942c5ad40765f1a6084dfe8e50cc4b9eb8b62f
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-10 15:46:27 +0300

    added user_id to Reservation

--- app/api/endpoints/reservation.py
+++ app/api/endpoints/reservation.py
@@ -8,7 +8,9 @@ from app.api.validators import (
     check_reservation_intersections
 )
 from app.core.db import get_async_session
+from app.core.user import current_user
 from app.crud.reservation import reservation_crud
+from app.models import User
 from app.schemas.reservation import (
     ReservationCreate,
     ReservationDB,
@@ -22,6 +24,7 @@ router = APIRouter()
 async def create_reservation(
         reservation: ReservationCreate,
         session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
 ):
     await check_meeting_room_exists(
         reservation.meetingroom_id, session
@@ -32,7 +35,9 @@ async def create_reservation(
         **reservation.model_dump(), session=session
     )
     new_reservation = await reservation_crud.create(
-        reservation, session
+        reservation,
+        session,
+        user
     )
     return new_reservation
 
