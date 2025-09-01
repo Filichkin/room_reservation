921a7055fc97d10cc20bd7722453b6ad15b6befb
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-09-01 20:40:36 +0300

    upd get_report

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -7,6 +7,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
+from app.services.google_api import (
+    create_spreadsheets,
+    set_user_permissions,
+    update_spreadsheets_value
+    )
 
 from app.crud.reservation import reservation_crud
 
@@ -31,4 +36,10 @@ async def get_report(
     reservations = await reservation_crud.get_count_res_at_the_same_time(
         from_reserve, to_reserve, session
     )
+
+    spreadsheetid = await create_spreadsheets(wrapper_services)
+    await set_user_permissions(spreadsheetid, wrapper_services)
+    await update_spreadsheets_value(spreadsheetid,
+                                    reservations,
+                                    wrapper_services)
     return reservations
